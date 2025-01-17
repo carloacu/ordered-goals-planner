fb9ed559215cb5c95330cc5c8af5f07e88b741cc
carloacu <carloacu.dev@gmail.com>
2025-01-17 15:31:11 +0100

    Add a duration test

--- tests/src/test_duration.cpp
+++ tests/src/test_duration.cpp
@@ -6,25 +6,11 @@
 
 namespace
 {
-const std::map<ogp::SetOfEventsId, ogp::SetOfEvents> _emptySetOfEvents;
 const std::unique_ptr<std::chrono::steady_clock::time_point> _now = {};
 const ogp::SetOfCallbacks _emptyCallbacks;
 
 
 static const std::vector<ogp::Parameter> _emptyParameters;
-
-
-ogp::Fact _fact(const std::string& pStr,
-                const ogp::Ontology& pOntology,
-                const ogp::SetOfEntities& pObjects,
-                const std::vector<ogp::Parameter>& pParameters = {}) {
-  return ogp::Fact(pStr, false, pOntology, pObjects, pParameters);
-}
-
-ogp::Parameter _parameter(const std::string& pStr,
-                          const ogp::Ontology& pOntology) {
-  return ogp::Parameter::fromStr(pStr, pOntology.types);
-}
 
 
 ogp::Goal _pddlGoal(const std::string& pStr,
@@ -58,35 +44,6 @@ void _setGoalsForAPriority(ogp::Problem& pProblem,
   pProblem.goalStack.setGoals(pGoals, pProblem.worldState, pConstants, pProblem.objects, pNow, pPriority);
 }
 
-bool _hasFact(ogp::WorldState& pWorldState,
-              const std::string& pFactStr,
-              const ogp::Ontology& pOntology,
-              const ogp::SetOfEntities& pObjects) {
-  return pWorldState.hasFact(_fact(pFactStr, pOntology, pObjects));
-}
-
-
-void _addFact(ogp::WorldState& pWorldState,
-              const std::string& pFactStr,
-              ogp::GoalStack& pGoalStack,
-              const ogp::Ontology& pOntology,
-              const ogp::SetOfEntities& pObjects,
-              const std::map<ogp::SetOfEventsId, ogp::SetOfEvents>& pSetOfEvents = _emptySetOfEvents,
-              const std::unique_ptr<std::chrono::steady_clock::time_point>& pNow = {}) {
-  pWorldState.addFact(_fact(pFactStr, pOntology, pObjects), pGoalStack, pSetOfEvents, _emptyCallbacks, pOntology, pObjects, pNow);
-}
-
-
-void _removeFact(ogp::WorldState& pWorldState,
-                 const std::string& pFactStr,
-                 ogp::GoalStack& pGoalStack,
-                 const ogp::Ontology& pOntology,
-                 const ogp::SetOfEntities& pObjects,
-                 const std::map<ogp::SetOfEventsId, ogp::SetOfEvents>& pSetOfEvents = _emptySetOfEvents,
-                 const std::unique_ptr<std::chrono::steady_clock::time_point>& pNow = {}) {
-  pWorldState.removeFact(_fact(pFactStr, pOntology, pObjects), pGoalStack, pSetOfEvents, _emptyCallbacks, pOntology, pObjects, pNow);
-}
-
 
 ogp::ActionInvocationWithGoal _lookForAnActionToDoThenNotify(
     ogp::Problem& pProblem,
@@ -102,6 +59,14 @@ ogp::ActionInvocationWithGoal _lookForAnActionToDoThenNotify(
     return firstActionInPlan;
   }
   return ogp::ActionInvocationWithGoal("", std::map<ogp::Parameter, ogp::Entity>(), {}, 0);
+}
+
+
+std::string _solveStr(ogp::Problem& pProblem,
+                      const ogp::Domain& pDomain,
+                      const std::unique_ptr<std::chrono::steady_clock::time_point>& pNow = {})
+{
+  return ogp::planToStr(ogp::planForEveryGoals(pProblem, pDomain, pNow), ", ");
 }
 
 
@@ -140,6 +105,58 @@ void _veryTestInvolvingDuration()
 }
 
 
+void _pathWothSmalldurationsInsteadOfOnrBigDuration()
+{
+  const std::string action1 = "action1";
+  const std::string action2 = "action2";
+  const std::string action3 = "action3";
+  const std::string action4 = "action4";
+
+  std::vector<std::pair<ogp::Number, std::string>> act2DurationToExpectedPlan{
+    {1, "action2"},
+    {2, "action2"},
+    {4, "action1, action3, action4"}
+  };
+
+  for (const auto& currTestParams : act2DurationToExpectedPlan)
+  {
+    const auto& act2Duration = currTestParams.first;
+    const auto& expectedPlan = currTestParams.second;
+
+    ogp::Ontology ontology;
+    ontology.predicates = ogp::SetOfPredicates::fromStr("fact_a\n"
+                                                        "fact_b\n"
+                                                        "fact_c", ontology.types);
+
+    std::map<std::string, ogp::Action> actions;
+    ogp::Action actionObj1({}, _worldStateModification_fromPddl("(fact_a)", ontology));
+    actionObj1.duration = 1;
+    actions.emplace(action1, actionObj1);
+
+    ogp::Action actionObj2({},
+                           _worldStateModification_fromPddl("(and (fact_a) (fact_b) (fact_c))", ontology));
+    actionObj2.duration = act2Duration;
+    actions.emplace(action2, actionObj2);
+
+    ogp::Action actionObj3({},
+                           _worldStateModification_fromPddl("(fact_b)", ontology));
+    actionObj3.duration = 1;
+    actions.emplace(action3, actionObj3);
+
+    ogp::Action actionObj4({},
+                           _worldStateModification_fromPddl("(fact_c)", ontology));
+    actionObj4.duration = 1;
+    actions.emplace(action4, actionObj4);
+
+    ogp::Domain domain(std::move(actions), ontology);
+    ogp::Problem problem;
+    _setGoalsForAPriority(problem, {_pddlGoal("(and (fact_a) (fact_b) (fact_c))", ontology)}, ontology.constants);
+
+    EXPECT_EQ(expectedPlan, _solveStr(problem, domain)) << "for action 2 duration: " + ogp::numberToString(act2Duration);
+  }
+}
+
+
 }
 
 
@@ -147,4 +164,5 @@ void _veryTestInvolvingDuration()
 TEST(Planner, test_duration)
 {
   _veryTestInvolvingDuration();
+  _pathWothSmalldurationsInsteadOfOnrBigDuration();
 }
